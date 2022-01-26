95107db55a67257829e642e37cc2fe975870a981
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-01-26 17:54:28 +0530

    Welcome home.

--- src/views/WelcomeHome.js
+++ src/views/WelcomeHome.js
@@ -1,7 +1,30 @@
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function WelcomeHome() {
+  let navigate = useNavigate();
+
+  const logout = () => {
+    navigate("/login");
+  };
+
   return (
-    <div>
-      <h1>Welcome Home</h1>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#home">Mini Project</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>Explore</Nav.Link>
+              <Nav.Link>Messages</Nav.Link>
+              <Nav.Link>Notification</Nav.Link>
+              <Nav.Link onClick={logout}>Logout</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
